e643112c37985a415f4a0df6ffa20a0249423338
George McDonnell <g.a.mcdonnell7@gmail.com>
2015-09-28 20:09:06 +0100

    Initial Commit

---
+++ Hasami Shogi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hasami Shogi
+//
+//  Created by George McDonnell on 28/09/2015.
+//  Copyright © 2015 George McDonnell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Hasami ShogiTests/Hasami_ShogiTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hasami_ShogiTests.swift
+//  Hasami ShogiTests
+//
+//  Created by George McDonnell on 28/09/2015.
+//  Copyright © 2015 George McDonnell. All rights reserved.
+//
+
+import XCTest
+@testable import Hasami_Shogi
+
+class Hasami_ShogiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
